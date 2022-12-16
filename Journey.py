c5dee064236c6427260115725462199584218e03
Ar-i-an <512643@vistacollege.nl>
2022-12-16 09:33:55 +0100

    Journey-start

---
+++ Journey.py
@@ -0,0 +1,42 @@
+percentage_chance = 0.36
+
+if some_function(percentage_chance):
+   # action here has 36% chance to execute
+   pass
+7
+import random
+
+if random.random() < percentage_chance:
+    print('aaa')
+
+
+    #Backstory for the story
+
+    #print("Backstory")
+    #print("You are a vikingnamed Jormungander, looking for a place to settledown")
+    #print("You want to stop the killing and live a good life without the need of slaughter")
+
+
+    import random
+
+    if random.random() < percentage_chance:
+        print('aaa')
+
+    #def introScene():
+    #directions = ["left","right","forward"]
+    #print("You start your journey in a forest. You are lost and do not know where to go.")
+    #print("The only direction you know is that you have to go North to reach your destination.")
+    #userInput = ""
+    #while userInput not in directions:
+    #print("Options: North/East/South/West")
+    #userInput = input()
+    #if userInput == "North":
+    #showShadowFigure()
+    #elif userInput == "East":
+    #showSkeletons()
+    #elif userInput == "South":
+    #hauntedRoom()
+    #elif userInput == "West":
+    #print("You find that this door opens into a wall.")
+    #else: 
+    #print("Please enter a valid option.")
